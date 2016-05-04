69dade3c74808f2ce9e7fd4e9dedced165561ca1
emRae <emRae@users.noreply.github.com>
2016-05-04 17:44:56 -0600

    nyc citibit 1hr usage script... haven't tested if this works yet.

--- unit-3/nyc-citibike-1hr-usage.py
+++ unit-3/nyc-citibike-1hr-usage.py
@@ -1,6 +1,8 @@
 
 # Import the requests package to handle data from online
 import requests
+import sqlite3 as lite
+import pandas as pd
 
 # get the NYC Citibike stations data
 r = requests.get('http://www.citibikenyc.com/stations/json')
@@ -17,12 +19,13 @@ for station in r.json()['stationBeanList']:
 from pandas.io.json import json_normalize
 df = json_normalize(r.json()['stationBeanList'])
 
-import sqlite3 as lite
+
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
 with con:
+    cur.execute('DROP TABLE IF EXISTS citibike_reference')
     cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
 
 #a prepared SQL statement we're going to execute over and over again
@@ -43,6 +46,7 @@ station_ids = ['_' + str(x) + ' INT' for x in station_ids]
 #create the table
 #in this case, we're concatenating the string and joining all the station ids (now with '_' and 'INT' added)
 with con:
+    cur.execute('DROP TABLE IF EXISTS available_bikes')
     cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
 
 
@@ -72,3 +76,55 @@ for station in r.json()['stationBeanList']:
 with con:
     for k, v in id_bikes.iteritems():
         cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+
+
+for i in range(60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime']).strftime("%s")
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
+
+    for station in r.json()['stationBeanList']:
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" % (station['id'], station['availableBikes'], exec_time))
+    con.commit()
+
+    time.sleep(60)
+
+con.close() #close the database connection when done
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+df1hr = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
+
+hour_change = collections.defaultdict(int)
+for col in df1hr.columns:
+    station_vals = df1hr[col].tolist()
+    station_id = col[1:] #trim the "_"
+    station_change = 0
+    for k,v in enumerate(station_vals):
+        if k < len(station_vals) - 1:
+            station_change += abs(station_vals[k] - station_vals[k+1])
+    hour_change[int(station_id)] = station_change #convert the station id back to integer
+
+def keywithmaxval(d):
+    """Find the key with the greatest value"""
+    return max(d, key=lambda k: d[k])
+
+# assign the max key to max_station
+max_station = keywithmaxval(hour_change)
+
+#query sqlite for reference information
+cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+print("The most active station is station id %s at %s latitude: %s longitude: %s " % data)
+print("With %d bicycles coming and going in the hour between %s and %s" % (
+    hour_change[max_station],
+    datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S'),
+    datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S'),
+))
+
+import matplotlib.pyplot as plt
+
+plt.bar(hour_change.keys(), hour_change.values())
+plt.show()
